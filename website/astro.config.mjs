547aca5855969376f6c100ed8ae38e3aaf9deb71
learjet5 <1971905575@qq.com>
2026-04-03 16:01:26 -0500

    Prepare deployment.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://systemsecurity-uiuc.github.io',
+  base: '/safeiot-website',
 });
+
