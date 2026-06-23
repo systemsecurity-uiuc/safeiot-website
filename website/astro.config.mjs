45235b57e78e0c0d0c6daf2eb098ffb1be6f3ace
luyixing <xingluyi@gmail.com>
2026-06-22 21:00:30 -0500

    Configure safe-iot.com custom domain

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://systemsecurity-uiuc.github.io',
-  base: '/safeiot-website',
+  site: 'https://safe-iot.com',
 });
 
